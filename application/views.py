874d1e7fbf8fd2415dff652ae8916b579342adf0
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-23 17:04:19 +0200

    layout

--- application/views.py
+++ application/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'application/index.html')
+
+def profile(request):
+    return render(request, 'application/profile.html')
