02037d88b6a19222a0fd50b6bfda918c681be1c1
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-23 17:17:32 +0200

    media and static

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+let searchForm = document.getElementById('searchForm')
+let pageLinks = document.getElementsByClassName('page-link')
+
+if (searchForm) {
+    for (let i = 0; pageLinks.length > i; i++) {
+        pageLinks[i].addEventListener('click', function(e) {
+            e.preventDefault()
+            console.log('button clicked')
+            let page = this.dataset.page
+            searchForm.innerHTML += `<input value="${page}" name="page" hidden/>`
+            searchForm.submit()
+        })
+    }
+}
+
+});
