6b10e5a2e9650ab990d58210deee14db858d557a
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-25 17:21:01 +0200

    login

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // let searchForm = document.getElementById('searchForm')
+    // let pageLinks = document.getElementsByClassName('page-link')
 
-let searchForm = document.getElementById('searchForm')
-let pageLinks = document.getElementsByClassName('page-link')
+    // if (searchForm) {
+    //     for (let i = 0; pageLinks.length > i; i++) {
+    //         pageLinks[i].addEventListener('click', function (e) {
+    //             e.preventDefault()
+    //             console.log('button clicked')
+    //             let page = this.dataset.page
+    //             searchForm.innerHTML += `<input value="${page}" name="page" hidden/>`
+    //             searchForm.submit()
+    //         })
+    //     }
+    // }
 
-if (searchForm) {
-    for (let i = 0; pageLinks.length > i; i++) {
-        pageLinks[i].addEventListener('click', function(e) {
-            e.preventDefault()
-            console.log('button clicked')
-            let page = this.dataset.page
-            searchForm.innerHTML += `<input value="${page}" name="page" hidden/>`
-            searchForm.submit()
-        })
-    }
-}
-
+    setTimeout(function () {
+        $(".alert").delay(1000).slideUp(200, function () {
+            $(this).alert('close');
+        });
+    }, 3000);
 });
