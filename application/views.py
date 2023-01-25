3c8f632d3319f7c866a5dbd717049ee7ef1fcc88
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-25 11:33:48 +0200

    profile card styled

--- application/views.py
+++ application/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
 
 def index(request):
-    return render(request, 'application/index.html')
+    profiles = Profile.objects.all()
+    context = {
+        "profiles": profiles
+    }
+    return render(request, 'application/index.html', context)
 
 def profile(request):
     return render(request, 'application/profile.html')
