c4c96cf5bfef17f7ed64d7a1af3f613f5947e3d5
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-27 13:54:46 +0200

    authentication

--- application/signals.py
+++ application/signals.py
@@ -1,26 +1,27 @@
 from django.db.models.signals import post_save, post_delete
-from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import User, Profile
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile(
-            user=instance, username=instance.username,
-            email=instance.email, name=instance.first_name)
+            user=instance, email=instance.email, name=instance.name)
         profile.save()
+
 
 @receiver(post_save, sender=Profile)
 def update_user(sender, instance, created, **kwargs):
     user = instance.user
     if created == False:
         user.first_name = instance.name
-        user.username = instance.username
         user.email = instance.email
         user.save()
+
 
 @receiver(post_delete, sender=Profile)
 def delete_user(sender, instance, **kwargs):
     user = instance.user
     user.delete()
+
--- core/settings.py
+++ core/settings.py
@@ -56,9 +56,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -66,9 +63,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -86,9 +80,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -97,9 +88,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
@@ -111,7 +99,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'application.User'
