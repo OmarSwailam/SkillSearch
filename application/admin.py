e27559f7aaf10e23a3426ae4ac7d0b5da2688328
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-29 11:01:35 +0200

    admin inline

--- application/admin.py
+++ application/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import User, Profile, Skill
 
 admin.site.register(User)
-admin.site.register(Profile)
 admin.site.register(Skill)
+
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [
+        SkillInline,
+    ]
+
+admin.site.register(Profile, ProfileAdmin)
