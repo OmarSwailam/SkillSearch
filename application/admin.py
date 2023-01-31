becc6e450d234195614d3f727fe03685b9322d07
OmarAbdelrazek <72394727+OmarSwailam@users.noreply.github.com>
2023-01-31 12:09:15 +0200

    submit multi image

--- application/admin.py
+++ application/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Profile, Skill
+from .models import User, Profile, Skill, Project, ProjectImage
 
 admin.site.register(User)
 admin.site.register(Skill)
@@ -8,9 +8,29 @@ admin.site.register(Skill)
 class SkillInline(admin.TabularInline):
     model = Skill
 
+
+class ProjectInline(admin.StackedInline):
+    model = Project
+
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+
+
 class ProfileAdmin(admin.ModelAdmin):
     inlines = [
         SkillInline,
+        ProjectInline,
     ]
 
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        ProjectImageInline,
+    ]
+
+
+
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Project, ProjectAdmin)
