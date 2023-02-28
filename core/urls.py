796cbf317c0716a408895ecdea3ad6c7c70b8c2a
yusufadell <yusufadell.dev@gmail.com>
2023-02-28 21:19:54 +0200

    chore: added configs for new pacakges

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('application.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("application.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
